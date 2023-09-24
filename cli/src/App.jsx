5c5f22fc34ef3c0cf5e96661af206b2307520a74
nayem islam <gangsn490@gmail.com>
2023-09-24 22:54:25 +0600

    updates

---
+++ cli/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Error from './components/Error';
+import Success from './components/Success';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/success' element={<Success />} />
+          <Route path='/error?' element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
